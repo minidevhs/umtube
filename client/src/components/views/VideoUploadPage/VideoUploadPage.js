c3f80b74c06374392c210f378a5f41f2e872ad42
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-03-25 17:50:30 +0900

    # 비디오 업로드

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -19,7 +20,8 @@ const CategoryOptions = [
   { value: 4, label: "Daily Life" },
 ];
 
-function VideoUploadPage() {
+function VideoUploadPage(props) {
+  const user = useSelector((state) => state.user);
   const [VideoTitle, setVideoTitle] = useState("");
   const [Description, setDescription] = useState("");
   const [Privacy, setPrivacy] = useState(0);
@@ -76,13 +78,40 @@ function VideoUploadPage() {
     });
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const variables = {
+      writer: user.userData._id,
+      title: VideoTitle,
+      description: Description,
+      privacy: Privacy,
+      filePath: FilePath,
+      category: Category,
+      duration: Duration,
+      thumbnail: ThumbnailPath,
+    };
+
+    axios.post("/api/video/uploadVideo", variables).then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        message.success("성공적으로 업로드를 했습니다.");
+        setTimeout(() => {
+          props.history.push("/");
+        }, 3000);
+      } else {
+        alert("비디오 업로드에 실패 했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <Title level={2}>Upload Video</Title>
       </div>
 
-      <Form>
+      <Form onSubmit={onSubmit}>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* Drop Zone */}
           <Dropzone
@@ -151,7 +180,7 @@ function VideoUploadPage() {
         <br />
         <br />
 
-        <Button type="primary" size="large">
+        <Button type="primary" size="large" onClick={onSubmit}>
           Submit
         </Button>
       </Form>
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const { Video } = require("../models/Video");
+const { Video } = require("../models/Video");
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
 const path = require("path");
@@ -40,6 +40,16 @@ router.post("/uploadfiles", (req, res) => {
       url: res.req.file.path,
       fileName: res.req.file.filename,
     });
+  });
+});
+
+router.post("/uploadVideo", (req, res) => {
+  // 모든 비디오 정보들을 저장
+  const video = new Video(req.body);
+
+  video.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({ success: true });
   });
 });
 
